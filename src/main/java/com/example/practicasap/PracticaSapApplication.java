30f9e0df7ec3cf90020d60dd0ca1eb463533cf01
GhibranLC <ghibranlpez16@hotmail.com>
2024-10-03 15:40:48 -0600

    Primer commit

---
+++ src/main/java/com/example/practicasap/PracticaSapApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicasap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaSapApplication.class, args);
+    }
+
+}
